b0d147b2e9e4a1b5c0d8a974d99cb3d3d60860f0
PHILADELPHIE <philadelphie@example.com>
2026-08-14 18:43:19 -0400

    chore: force business hours open for 30 minutes for testing

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ async function sendAutoReply(callerNumber) {
 function isBusinessHours() {
     try {
         const now = new Date();
-        if (now.getTime() < 1786486224032) return true; // Temporarily open for testing
+        if (now.getTime() < 1786663000000) return true; // Temporarily open for testing
 
         const month = now.getUTCMonth(); 
         let offset = -5;
